03e18a72491ef255cc01116866e0b0050b876b11
Radovan Svedic <radovans@vast.com>
2014-06-22 10:58:40 +0200

    Continued development of POC for Sticker Exchanger app

--- js/exchanger.js
+++ js/exchanger.js
@@ -1,5 +1,57 @@
 var Xchanger = Xchanger || {};
 
+// this way stickers will take less space in both memory and storage:
+Xchanger.stickersFields = {
+    label: 'l',
+    count: 'c',
+    position: 'p'
+};
+
+Xchanger.dictionary = {
+    phrasses: {
+        'failed_to_store_to_local_storage': {
+            eng: 'Failed to store current sticker\'s set to device, please try with restart of software or device, there\'s a possibility that this problem may dissapear that way. Thank you!', 
+            srb: 'Скуп сличица није успешно сачуван на уређају, молимо Вас да поново покушате да покренете програм или сам уређај, постоји могућност да ће на такав начин овај проблем бити елиминисан. Хвала!'
+        }
+    },
+    words: {
+        'all_stickers': {
+            eng: 'All', 
+            srb: 'Све'
+        },
+        'missing_stickers': {
+            eng: 'Missing', 
+            srb: 'Недостајуће'
+        },
+        'duplicate_stickers': {
+            eng: 'Duplicates', 
+            srb: 'Дупликати'
+        },
+        'found_stickers': {
+            eng: 'Found', 
+            srb: 'Попуњене'
+        },
+    }
+};
+
+Xchanger.language = 'srb';
+
+var storeStickers = function(stickers, key) {
+    try {
+        window.localStorage.setItem(key, JSON.stringify(stickers));
+    }
+    catch(e) {
+        console.log('Failed to store to localStorage with the following error message:', e.message);
+        
+        alert(Xchanger.dictionary.phrasses.failed_to_store_to_local_storage[Xchanger.language]);
+    }
+};
+
+var getStoredStickers = function(key) {key
+    
+};
+
+
 var generateStickers = function() {
     Xchanger.stickers = Xchanger.sticker || [];
 
@@ -15,33 +67,41 @@ var generateStickers = function() {
     }
     else {
 
-        var pos = 0;
+        var position = 0;
         
         // ad descriptive sticker:
-        Xchanger.stickers.push({label: '00', count: -1, pos: pos++});
+        var sticker = {};
+            sticker[Xchanger.stickersFields.label] = '00';
+            sticker[Xchanger.stickersFields.count] = -1;
+            sticker[Xchanger.stickersFields.position] = position++;        
+        Xchanger.stickers.push(sticker);
 
-        // add numeric stickers:    
+        // add numeric stickers:
         var i;
         for(i=1; i<640; i++) {
-            Xchanger.stickers.push({label: i.toString(), count: -1, pos: pos++});
+            sticker = {};
+                sticker[Xchanger.stickersFields.label] = i.toString();
+                sticker[Xchanger.stickersFields.count] = -1;
+                sticker[Xchanger.stickersFields.position] = position++;        
+            Xchanger.stickers.push(sticker);
         }
     } 
-}
+};
 
 var drawStickers = function() {
     var i;
     for(i in Xchanger.stickers) {
         var currentStickerState = 'sticker-missing';
-        if (Xchanger.stickers[i].count === 0) {
+        if (Xchanger.stickers[i][Xchanger.stickersFields.count] === 0) {
             currentStickerState = 'sticker-found';
         }
-        else if (Xchanger.stickers[i].count > 0) {
+        else if (Xchanger.stickers[i][Xchanger.stickersFields.count] > 0) {
             currentStickerState = 'sticker-duplicate';
         }
 
-        $('.container').append('<div id="' + Xchanger.stickers[i].label + '" class="sticker sticker-pointable ' + currentStickerState + '" data-pos="' + Xchanger.stickers[i].pos + '">' + Xchanger.stickers[i].label + '</div>');
+        $('.container').append('<div id="' + Xchanger.stickers[i][Xchanger.stickersFields.label] + '" class="sticker sticker-pointable ' + currentStickerState + '" data-pos="' + Xchanger.stickers[i][Xchanger.stickersFields.position] + '">' + Xchanger.stickers[i][Xchanger.stickersFields.label] + '</div>');
     }
-}
+};
 
 
 
@@ -85,27 +145,27 @@ $('.sticker').on('click', function(){
                 
                 // reduce current sticker's count by 1
                 // but make sure it's never less than -1
-                if (stickerModel.count > -1) {
-                    stickerModel.count -= 1;
+                if (stickerModel[Xchanger.stickersFields.count] > -1) {
+                    stickerModel[Xchanger.stickersFields.count] -= 1;
                 }
             }
             else if ($(this).hasClass('sticker-add-one')) {
                 console.log('... add one sticker');
 
                 // increment current sticker's count by 1
-                stickerModel.count += 1; 
+                stickerModel[Xchanger.stickersFields.count] += 1; 
             }
             
             // update storage with the most recent data:
-            window.localStorage.setItem('stickerXchanger', JSON.stringify(Xchanger.stickers));
+            storeStickers(Xchanger.stickers, 'stickerXchanger');
 
             // ... then check current sticker's state (count), after previous action:
             // add/remove appropriate classes:
-            if (stickerModel.count > 0) {
+            if (stickerModel[Xchanger.stickersFields.count] > 0) {
                 actionItem.addClass('sticker-duplicate');
                 actionItem.removeClass('sticker-missing sticker-found');             
             }
-            else if (stickerModel.count === 0) {
+            else if (stickerModel[Xchanger.stickersFields.count] === 0) {
                 actionItem.addClass('sticker-found');                
                 actionItem.removeClass('sticker-missing sticker-duplicate');             
             }
@@ -164,4 +224,11 @@ $('.stickers_tabs a').on('click', function(e) {
         $('.sticker-found').show();        
     }
     
-})
+});
+
+
+// initialize some language phrasses:
+$('.stickers-tab-all').text(Xchanger.dictionary.words.all_stickers[Xchanger.language]);
+$('.stickers-tab-missing').text(Xchanger.dictionary.words.missing_stickers[Xchanger.language]);
+$('.stickers-tab-duplicates').text(Xchanger.dictionary.words.duplicate_stickers[Xchanger.language]);
+$('.stickers-tab-found').text(Xchanger.dictionary.words.found_stickers[Xchanger.language]);
